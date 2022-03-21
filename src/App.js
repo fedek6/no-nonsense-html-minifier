fce0c52a4ff15a095a99ecea6e574abe97dbec83
Konrad Fedorczyk <fedek6@gmail.com>
2022-03-21 13:27:41 +0100

    Add fully working code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,74 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const compress = (markup) => {
+  const output = markup
+    .replace(/\n/g, "")
+    .replace(/[\t ]+</g, "<")
+    .replace(/>[\t ]+</g, "><")
+    .replace(/>[\t ]+$/g, ">");
+
+  return output;
+};
+
+const selectAndCopy = (target, callback) => {
+  target.select();
+  navigator.clipboard.writeText(target.value).then(callback);
+};
 
 function App() {
+  const [original, setOriginal] = useState("");
+  const [minified, setMinified] = useState("");
+  const [copied, setCopied] = useState(false);
+
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => setCopied(false), 500);
+    }
+  }, [copied]);
+
+  const handleOriginal = (e) => {
+    setOriginal(e.target.value);
+    setMinified(compress(e.target.value));
+  };
+
   return (
     <div className="App">
-      Lorem iposumg
+      <header>
+        <h1>No nonsense HTML minifier</h1>
+        <figure>
+          <blockquote cite="https://dictionary.cambridge.org/dictionary/english/no-nonsense">
+            Practical and serious, and only interested in doing what is
+            necessary or achieving what is intended, without silly ideas or
+            methods
+          </blockquote>
+          <figcaption>Cambridge Dictionary</figcaption>
+        </figure>
+      </header>
+      <main>
+        <div>
+          <h2>Paste code here:</h2>
+          <textarea
+            value={original}
+            onChange={handleOriginal}
+            onClick={(e) => e.target.select()}
+          />
+        </div>
+        <div>
+          <h2>
+            Minified code <small>click to copy</small>:
+          </h2>
+          <textarea
+            value={minified}
+            onClick={(e) => {
+              selectAndCopy(e.target, () => {
+                setCopied(true);
+              });
+            }}
+          />
+          {copied && <strong className="ok">Copied!</strong>}
+        </div>
+      </main>
     </div>
   );
 }
